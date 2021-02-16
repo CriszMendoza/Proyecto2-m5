1074c592efa65ed673adb1b4fb1eedd386eeabcd
CriszMendoza <criszmendoza212@gmail.com>
2021-02-16 15:22:27 -0600

    "JS"

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cuentas = [
+let cuentas = [
   { 
     nombre: "Hiromi", 
     saldo: 200, 
@@ -16,17 +16,67 @@ var cuentas = [
   }
 ];
 
-const $btnRegistro = document.getElementById("btn-registro")
+function getDatos(){
+  let user = document.getElementById("user").value,
+      password = document.getElementById("password").value,
+      $form = document.getElementById("form-alert")
 
-const getData = () =>{
-  let usuario = {
-    nombre: document.getElementById("nombre").name,
-    contraseña: document.getElementById("contraseña").name,
-    monto: document.getElementById("monto").name
+  if (user === "" || password === "") {
+    return false
+  } else{
+    for (let i = 0; i < cuentas.length; i++) {
+      if (user === cuentas[i].nombre && password === cuentas[i].password){
+    
+        return incrustraHTML(cuentas[i].nombre,cuentas[i].saldo)
+      }else {
+        return $form.outerHTML = `
+        <div class="alert alert-danger" role="alert">
+          La contraseña que ingresaste es incorrecta
+        </div>
+        `
+      }
+    }  
   }
-  cuentas.push(usuario)
 }
 
-console.log(cuentas);
-$btnRegistro.addEventListener("click", getData)
+function incrustraHTML (nombre) {
+  $('#modal-registro').modal('hide')
+  $fragment = document.createDocumentFragment()
+  $main = document.getElementById("incrustar"),
+  $main.outerHTML=`
+  <div class="container text-light pt-5 pb-5">
+      <div>
+        <h2 id="benvenida">Hola ${nombre} Bienvenid@!</h2>
+      </div>
+      <div>
+        <h4 class="mb-4">¿Que deseas hacer hoy?</h4>
+        <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#consulta-saldo">Consultar saldo</button>
+        <button type="button" class="btn btn-secondary" onclick="ingresarMonto()">Ingresar monto</button>
+        <button type="button" class="btn btn-secondary" onclick="retirarMonto()" >Retirar monto</button>
+      </div>
+    </div>
+  `
+}
 
+let usuario;
+
+function sesionIniciada (){
+  let user = document.getElementById("user").value,
+      password = document.getElementById("password").value
+  for (let i = 0; i < cuentas.length; i++) {
+    if (user === cuentas[i].nombre && password === cuentas[i].password) {
+      usuario = cuentas[i] 
+    }
+  }
+}
+
+function consultarSaldo(){
+  let $modalSaldo = document.getElementById("modal-saldo")
+  $modalSaldo.textContent =`Tu saldo disponible es de: $${usuario.saldo}.00`
+}
+
+let $btnRegistro = document.getElementById("btn-enviar")
+$btnRegistro.addEventListener("click", getDatos)
+$btnRegistro.addEventListener("click", sesionIniciada)
+let $consultarSaldo = document.getElementById("consultarSaldo")
+$consultarSaldo.addEventListener("click",consultarSaldo)
